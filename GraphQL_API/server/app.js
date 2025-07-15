a3475f9056913829cbd692c50afa3eed7118af22
kevxcyj <kevcyjunior@gmail.com>
2025-07-14 20:31:31 -0500

    Updated app

--- GraphQL_API/server/app.js
+++ GraphQL_API/server/app.js
@@ -1,10 +1,14 @@
 const express = require('express');
-const {graphqlHTTP} = require('express-graphql');
+const { graphqlHTTP } = require('express-graphql');
+const TaskType = require('./schema');
 
 const app = express();
 
-app.use('/graphql',graphqlHTTP({
+app.use('/graphql', graphqlHTTP({
+  schema: TaskType,
+  graphiql: true,
 }));
-app.listen(4000,()=>{
-  console.log('now listening for request on port 4000');
+
+app.listen(4000, () => {
+  console.log('Now listening for requests on port 4000');
 });
---
+++ GraphQL_API/server/schema/schema.js
@@ -0,0 +1,13 @@
+const { GraphQLObjectType, GraphQLString, GraphQLInt } = require('graphql');
+
+const TaskType = new GraphQLObjectType({
+  name: 'Task',
+  fields: {
+    id: { type: GraphQLString },
+    title: { type: GraphQLString },
+    weight: { type: GraphQLInt },
+    description: { type: GraphQLString }
+  }
+});
+
+module.exports = TaskType;
