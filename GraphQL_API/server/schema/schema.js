bfefa8043b6f174c9888a3483fd4df8729452c24
kevxcyj <kevcyjunior@gmail.com>
2025-07-23 19:45:14 -0500

    Updated schemajs for lodash

--- GraphQL_API/server/schema/schema.js
+++ GraphQL_API/server/schema/schema.js
@@ -1,5 +1,21 @@
 import { GraphQLObjectType, GraphQLString, GraphQLInt } from 'graphql';
 import { GraphQLSchema } from 'graphql';
+import lodash from 'lodash';
+
+const tasks = [
+  {
+    id: '1',
+    title: 'Create your first webpage',
+    weight: 1,
+    description: 'Create your first HTML file 0-index.html with: -Add the doctype on the first line (without any comment) -After the doctype, open and close a html tag Open your file in your browser (the page should be blank)',
+  },
+  {
+    id: '2',
+    title: 'Structure your webpage',
+    weight: 1,
+    description: 'Copy the content of 0-index.html into 1-index.html Create the head and body sections inside the html tag, create the head and body tags (empty) in this order',
+  },
+]
 
 const TaskType = new GraphQLObjectType({
   name: 'Task',
@@ -20,7 +36,7 @@ const RootQueryType = new GraphQLObjectType({
         id: { type: GraphQLString },
       },
       resolve: (parent, args) => {
-        return {}
+        return lodash.find(tasks, {id: args.id});
       },
     },
   }),
