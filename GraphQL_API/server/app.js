4d469da9a04cf3b32e72a9e3230103b33a874df2
kevxcyj <kevcyjunior@gmail.com>
2025-07-23 19:08:42 -0500

    updated schema

--- GraphQL_API/server/app.js
+++ GraphQL_API/server/app.js
@@ -10,6 +10,6 @@ app.use('/graphql', graphqlHTTP({
   schema: schema,
 }));
 
-app.listen(PORT, () => {
+app.listen(4000, () => {
   console.log(`now listening for request on port ${4000}`);
 });
--- GraphQL_API/server/schema/schema.js
+++ GraphQL_API/server/schema/schema.js
@@ -1,6 +1,17 @@
 import { GraphQLObjectType, GraphQLString, GraphQLInt } from 'graphql';
 
+const TaskType = new GraphQLObjectType({
+  name: 'Task',
+  fields: {
+    id: { type: GraphQLString },
+    title: { type: GraphQLString },
+    weight: { type: GraphQLInt },
+    description: { type: GraphQLString }
+  }
+});
+
+// Export the schema
 export const schema = new GraphQLSchema({
-  query: RootQueryType,
-  mutation: Mutation,
+  query: TaskType,
+  mutation: null // Add mutations here if needed
 });
