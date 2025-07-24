e87ef5d3c798c4c756aa1e61ea47dd2b2b5303de
kevxcyj <kevcyjunior@gmail.com>
2025-07-23 20:19:07 -0500

    Updated schema

--- GraphQL_API/server/schema/schema.js
+++ GraphQL_API/server/schema/schema.js
@@ -8,12 +8,14 @@ const tasks = [
     title: 'Create your first webpage',
     weight: 1,
     description: 'Create your first HTML file 0-index.html with: -Add the doctype on the first line (without any comment) -After the doctype, open and close a html tag Open your file in your browser (the page should be blank)',
+    project: 1,
   },
   {
     id: '2',
     title: 'Structure your webpage',
     weight: 1,
     description: 'Copy the content of 0-index.html into 1-index.html Create the head and body sections inside the html tag, create the head and body tags (empty) in this order',
+    project: 1,
   },
 ]
 
