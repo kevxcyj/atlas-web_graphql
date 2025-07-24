80782362ce9da0075a90eaf7566be60bd6eef67c
kevxcyj <kevcyjunior@gmail.com>
2025-07-23 20:51:10 -0500

    updated appjs

--- GraphQL_API/server/app.js
+++ GraphQL_API/server/app.js
@@ -1,9 +1,25 @@
 import express from 'express';
 import { graphqlHTTP } from 'express-graphql';
 import { schema } from './schema/schema.js';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
 
 const app = express();
 const PORT = 4000
+
+const mongoUri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}
+@${process.env.MONGODB_CLUSTER}/${process.env.MONGODB_DATABASE}
+/?appName=${process.env.MONGODB_APPNAME}`;
+
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.once('open', () => {
+  console.log('connected to database');
+});
 
 app.use('/graphql', graphqlHTTP({
   graphiql: true,
